1d7c0ff0a57f770322c2f0aafdf270da28e91db1
Charlie Hill <157416801+chill6363@users.noreply.github.com>
2024-10-12 22:43:34 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int costSpring = 1000;
+        int costSummer = 2000;
+        int costFall = 500;
+        int costWinter = 4000;
+        int costTotal = costSpring + costSummer + costFall + costWinter;
+
+        System.out.println("The total maintenance cost for the year is " + costTotal);
+    }
+}
